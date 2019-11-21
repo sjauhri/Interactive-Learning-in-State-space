e0e072bb6616dbbd2bca000c72e3110545a0a693
Snehal Jauhri <sjauhri@pop-os.localdomain>
2019-11-21 10:47:00 +0100

    Hopper debugging

--- MyBCO/scripts/run_bco_hopper.sh
+++ MyBCO/scripts/run_bco_hopper.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 models/bco_hopper.py --mode=train --input_file=demonstration/expert_obs/Hopper-v2.pkl --model_dir=model/hopper/ --premodel_dir=premodel/hopper/ --print_freq=1 --save_freq=1
+python3 models/bco_hopper.py --mode=train --input_file=demonstration/expert_obs/Hopper-v2.pkl --model_dir=model/hopper/ --premodel_dir=premodel/hopper/ --print_freq=1 --save_freq=1 --maxEpochs=100
