b46e8b3e899a6bd6b905e1e6cbccf112e35c208f
sjauhri <s.jauhri@student.tudelft.nl>
2020-03-18 18:09:46 +0100

    lunar test to be run

--- TIPS/models/tips_lunarlandercont_test2.py
+++ TIPS/models/tips_lunarlandercont_test2.py
@@ -1,40 +1,44 @@
 from utils import *
 from tips import TIPS
-from feedback import *
-from fdm_cartpole import *
+from feedback_lunar import *
+from fdm_lunarl import *
 import gym
 
-class TIPS_cartpole(TIPS):
-  def __init__(self, state_shape, action_shape, lr=0.0005, maxEpisodes=30, epochTrainIts=8000, dynamicsSamples=500, batch_size=16):
+class TIPS_lunarlandercont(TIPS):
+  def __init__(self, state_shape, action_shape, lr=0.0005, maxEpisodes=50, epochTrainIts=9000,  dynamicsSamples=5000, batch_size=32):
     TIPS.__init__(self, state_shape, action_shape, lr=lr, maxEpisodes=maxEpisodes, epochTrainIts=epochTrainIts, dynamicsSamples=dynamicsSamples, batch_size=batch_size)
 
     # set which game to play
-    self.env = gym.make('CartPole-v0')
+    self.env = gym.make('LunarLanderContinuous-v2')
     self.env.reset()
     self.env.render()  # Make the environment visible
 
     # Initialise Human feedback (call render before this)
-    self.human_feedback = Feedback(self.env)
+    self.human_feedback = Feedback_lunar(self.env)
     # Set error constant multiplier for this environment
-    # 0.01, 0.05, 0.1, 0.5
-    self.errorConst = 0.05#0.2
+    # 0.01, 0.05, 0.1, 0.5, 1
+    self.errorConst = 0.35
     # Render time delay for this environment (in s)
-    self.render_delay = 0.075#0.08
+    self.render_delay = 0.065
     # Feedback training rate in the episode
     self.feedback_training_rate  = 10
 
     # Choose which feedback is valid with fb dictionary
     self.feedback_dict = {
       H_NULL: 0,
-      H_UP: 0,
-      H_DOWN: 0,
+      H_UP: 1,
+      H_DOWN: 1,
       H_LEFT: 1,
       H_RIGHT: 1,
       H_HOLD: 0,
-      DO_NOTHING: 0
+      DO_NOTHING: 0,
+      H_UPLEFT: 1,
+      H_UPRIGHT: 1,
+      H_DOWNLEFT: 1,
+      H_DOWNRIGHT: 1
     }
 
-    self.ifdm_queries = 10 # Two discrete actions. (Easy)
+    self.ifdm_queries = 250 # Two continous actions.
 
   def build_policy_model(self):
     """buliding the policy model as two fully connected layers with leaky relu"""
@@ -42,18 +46,17 @@ class TIPS_cartpole(TIPS):
       with tf.variable_scope("input") as scope:
         policy_input = self.state
       with tf.variable_scope("model") as scope:
-        policy_h1 = tf.layers.dense(policy_input, 16, kernel_initializer=weight_initializer(), bias_initializer=bias_initializer(), name="dense_1")
+        policy_h1 = tf.layers.dense(policy_input, 32, kernel_initializer=weight_initializer(), bias_initializer=bias_initializer(), name="dense_1")
         policy_h1 = tf.nn.leaky_relu(policy_h1, 0.2, name="LeakyRelu_1")
-        policy_h2 = tf.layers.dense(policy_h1, 16, kernel_initializer=weight_initializer(), bias_initializer=bias_initializer(), name="dense_2")
+        policy_h2 = tf.layers.dense(policy_h1, 32, kernel_initializer=weight_initializer(), bias_initializer=bias_initializer(), name="dense_2")
         policy_h2 = tf.nn.leaky_relu(policy_h2, 0.2, name="LeakyRelu_2")
 
       with tf.variable_scope("output") as scope:
-        policy_pred_action = tf.layers.dense(policy_h2, self.action_dim, kernel_initializer=weight_initializer(), bias_initializer=bias_initializer(), name="dense")
-        self.tmp_policy_pred_action = policy_pred_action
-        self.policy_pred_action = tf.one_hot(tf.argmax(policy_pred_action, axis=1), self.action_dim, name="one_hot")
+        self.tmp_policy_pred_action = tf.layers.dense(policy_h2, self.action_dim, kernel_initializer=weight_initializer(), bias_initializer=bias_initializer(), name="dense")
+        self.policy_pred_action = tf.clip_by_value(self.tmp_policy_pred_action, clip_value_min=-1, clip_value_max=1)
 
       with tf.variable_scope("loss") as scope:
-        self.policy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.action, logits=policy_pred_action))
+        self.policy_loss = tf.reduce_mean(tf.squared_difference(self.policy_pred_action, self.action))
       with tf.variable_scope("train_step") as scope:
         self.policy_train_step = tf.train.AdamOptimizer(self.lr).minimize(self.policy_loss)
 
@@ -63,9 +66,9 @@ class TIPS_cartpole(TIPS):
       with tf.variable_scope("input") as scope:
         fdm_input = tf.concat([self.state, self.action], 1)
       with tf.variable_scope("model") as scope:
-        fdm_h1 = tf.layers.dense(fdm_input, 16, kernel_initializer=weight_initializer(), bias_initializer=bias_initializer(), name="dense_1")
+        fdm_h1 = tf.layers.dense(fdm_input, 64, kernel_initializer=weight_initializer(), bias_initializer=bias_initializer(), name="dense_1")
         fdm_h1 = tf.nn.leaky_relu(fdm_h1, 0.2, name="LeakyRelu_1")
-        fdm_h2 = tf.layers.dense(fdm_h1, 16, kernel_initializer=weight_initializer(), bias_initializer=bias_initializer(), name="dense_2")
+        fdm_h2 = tf.layers.dense(fdm_h1, 64, kernel_initializer=weight_initializer(), bias_initializer=bias_initializer(), name="dense_2")
         fdm_h2 = tf.nn.leaky_relu(fdm_h2, 0.2, name="LeakyRelu_2")
 
       with tf.variable_scope("output") as scope:                
@@ -89,18 +92,22 @@ class TIPS_cartpole(TIPS):
 
       prev_s = state
 
-      A = np.random.randint(self.action_dim)
-      a = np.zeros([self.action_dim])
-      a[A] = 1
+      # Continuos action space
+      # Actions between -1 and 1
+      A = np.random.uniform(-1, 1, self.action_dim)
 
       state, _, terminal, _ = self.env.step(A)
-      # x position frame correction!
-      state[0] -= prev_s[0]
-      prev_s[0] = 0
+      # x-y position frame correction!
+      state_mod = np.copy(state)
+      prev_s_mod = np.copy(prev_s)
+      state_mod[0] -= prev_s_mod[0]
+      state_mod[1] -= prev_s_mod[1]
+      prev_s_mod[0] = 0
+      prev_s_mod[1] = 0
 
-      States.append(prev_s)
-      Nstates.append(state)
-      Actions.append(a)
+      States.append(prev_s_mod)
+      Nstates.append(state_mod)
+      Actions.append(A)
 
       if i and (i+1) % 1000 == 0:
         print("Collecting dynamics training data ", i+1)
@@ -115,7 +122,7 @@ class TIPS_cartpole(TIPS):
     Nstates = []
     Actions = []
 
-    for i in range(int(round(self.dynamicsSamples/2))): # Using half the initial dynamics samples
+    for i in range(int(round(self.dynamicsSamples/10))): # Using 10% of the initial dynamics samples
       if terminal:
         state = self.env.reset()
 
@@ -123,24 +130,26 @@ class TIPS_cartpole(TIPS):
       state = np.reshape(state, [-1,self.state_dim])
 
       # Using an epsilon-greedy policy for exploration of new actions
-      if (np.random.uniform(0,1) < 0.15):
-        # Discrete action space
-        A = np.random.randint(self.action_dim)
-        a = np.zeros([self.action_dim])
-        a[A] = 1
+      if (np.random.uniform(0,1) < 0.1):
+        # Continuos action space
+        # Actions between -1 and 1
+        A = np.random.uniform(-1, 1, self.action_dim)
       else:
-        # Discrete action space
-        a = np.reshape(self.eval_policy(state), [-1])
-        A = np.argmax(a)
+        # Continuos action space
+        A = np.reshape(self.eval_policy(state), [-1])
 
       state, _, terminal, _ = self.env.step(A)
-      # x position frame correction!
-      state[0] -= prev_s[0]
-      prev_s[0] = 0
+      # x-y position frame correction!
+      state_mod = np.copy(state)
+      prev_s_mod = np.copy(prev_s)
+      state_mod[0] -= prev_s_mod[0]
+      state_mod[1] -= prev_s_mod[1]
+      prev_s_mod[0] = 0
+      prev_s_mod[1] = 0
 
-      States.append(prev_s)
-      Nstates.append(state)
-      Actions.append(a)
+      States.append(prev_s_mod)
+      Nstates.append(state_mod)
+      Actions.append(A)
 
       if i and (i+1) % 1000 == 0:
         print("Collecting dynamics training data from exploration policy ", i+1)
@@ -150,97 +159,112 @@ class TIPS_cartpole(TIPS):
 
   def get_state_corrected(self, h_fb, state):
     """get corrected state label for this environment using feedback"""
-    # state_corrected = np.copy(state)
-    state_corrected = np.zeros(2)
+    state_corrected = np.copy(state)
 
-    # Get auxiliary state: pole tip position
-    angle = (state[3]*np.pi)/180    
-    state_corrected[0] = np.sin(angle) # x
-    state_corrected[1] = np.cos(angle) # y
-
-    # IF CHANGING TYPE OF STATE FEEDBACK, ALSO CHANGE get_corrected_action()    
+    # IF CHANGING TYPE OF STATE FEEDBACK, ALSO CHANGE get_corrected_action()
     if (h_fb == H_LEFT):
-      # state_corrected[2] -= self.errorConst # Correcting Velocity
-      state_corrected[0] -= self.errorConst # Correcting Pole tip position
+      # state_corrected[3] = 0                  # Zero vertical velocity
+      state_corrected[5] += self.errorConst   # Angular velocity
     elif (h_fb == H_RIGHT):
-      # state_corrected[2] += self.errorConst # Correcting Velocity
-      state_corrected[0] += self.errorConst # Correcting Pole tip position
-    # else: # (h_fb == H_HOLD)
-    #   state_corrected[3] = 0  # HOLD pole angular velocity zero
-    
+      # state_corrected[3] = 0                  # Zero vertical velocity
+      state_corrected[5] -= self.errorConst   # Angular velocity
+    elif (h_fb == H_UP):
+      state_corrected[3] += self.errorConst   # Vertical velocity
+      # state_corrected[5] = 0                  # Zero angular velocity
+    elif (h_fb == H_UPLEFT):
+      state_corrected[3] += self.errorConst   # Vertical velocity
+      state_corrected[5] += self.errorConst   # Angular velocity
+    elif (h_fb == H_UPRIGHT):
+      state_corrected[3] += self.errorConst   # Vertical velocity
+      state_corrected[5] -= self.errorConst   # Angular velocity
+    elif (h_fb == H_DOWN):
+      state_corrected[3] -= self.errorConst   # Vertical velocity
+      # state_corrected[5] = 0                  # Zero angular velocity
+    elif (h_fb == H_DOWNLEFT):
+      state_corrected[3] -= self.errorConst   # Vertical velocity
+      state_corrected[5] += self.errorConst   # Angular velocity
+    elif (h_fb == H_DOWNRIGHT):
+      state_corrected[3] -= self.errorConst   # Vertical velocity
+      state_corrected[5] -= self.errorConst   # Angular velocity
+
     return state_corrected
 
   def get_corrected_action(self, h_fb, state, state_corrected):
     """get action to achieve next state close to state_corrected"""
 
-    if (args.learnFDM):
+    if (h_fb == H_DOWN):
+      min_action = np.array((-2,0)) # Dont fire any engine
+      if (args.learnFDM):
+        # Debug: equal timing
+        # time.sleep(0.02)
+        pass
+      else:
+        # Debug: equal timing
+        time.sleep(0.04)
+    elif (args.learnFDM):
+      # prev_time = time.time()
       # Learnt FDM:
 
-      # x position frame correction!
-      # state_corrected[0] -= state[0]
+      # x-y position frame correction!
+      state_corrected[0] -= state[0]
+      state_corrected[1] -= state[1]
       state[0] = 0
+      state[1] = 0
 
       # Make a vector of same states
       States = np.tile(state, (self.ifdm_queries,1))
       # Choose random actions
-      # Discrete Actions
-      a = np.eye(self.action_dim)
-      Actions = a[np.random.choice(a.shape[0], size=self.ifdm_queries)]
+      # Continuous Actions
+      Actions = np.random.uniform(-1, 1, (self.ifdm_queries,self.action_dim) )
       # Query ifdm to get next state
       Nstates = self.eval_fdm(States, Actions)
 
-      # Get auxiliary state: pole tip position
-      Nstates_ang = (Nstates[:,3]*np.pi)/180
-      Nstates_xpos = np.sin(Nstates_ang)
-      Nstates_ypos = np.cos(Nstates_ang)
-
       # Calculate cost
-      # if (h_fb == H_HOLD):
-      #   cost = abs(state_corrected[3] - Nstates[:,3]) # Corrected Pole Angular Velocity (Zero)
-      # else: # LEFT or RIGHT
-      # cost = abs(state_corrected[2] - Nstates[:,2]) # Corrected Velocity
-      cost = abs(state_corrected[0] - Nstates_xpos) + abs(state_corrected[1] - Nstates_ypos) # Corrected pole tip position
+      if (h_fb == H_LEFT or h_fb == H_RIGHT):
+        cost = abs(state_corrected[5] - Nstates[:,5])
+      elif (h_fb == H_UP or h_fb == H_DOWN):
+        cost = abs(state_corrected[3] - Nstates[:,3])
+      else:# (h_fb == H_UPLEFT or UPRIGHT or...):
+        cost = abs(state_corrected[3] - Nstates[:,3]) + abs(state_corrected[5] - Nstates[:,5])
+
 
       # Check for min_cost
       min_cost_index = cost.argmin(axis=0)
-      min_a = Actions[min_cost_index]
+      min_action = Actions[min_cost_index]
+
+      # Debug: equal timing
+      # print(time.time() - prev_time)
     else:
       # True FDM:
-
-      # Discrete Actions
-      min_action = np.random.randint(self.action_dim)
+      # Continous Actions
+      min_action = np.random.uniform(-1, 1, self.action_dim)
       min_cost = np.Inf
 
       for _ in range(1, self.ifdm_queries+1):
         # Choose random action
-        # Discrete actions
-        curr_action = np.random.randint(self.action_dim)
+        # Continous Actions
+        curr_action = np.random.uniform(-1, 1, self.action_dim)
+        # Discretization
+        # val_set = [0.2*x for x in range(-5,6)]
+        # curr_action = np.random.choice(val_set, self.action_dim)
 
         # Query ifdm to get next state
-        nstate = fdm(state, curr_action)
+        nstate = fdm_cont(state, curr_action)
 
-        # Get auxiliary state: pole tip position
-        nstate_ang = (nstate[3]*np.pi)/180
-        nstate_xpos = np.sin(nstate_ang)
-        nstate_ypos = np.cos(nstate_ang)
+        # Check cost        
+        if (h_fb == H_LEFT or h_fb == H_RIGHT):
+          cost = abs(state_corrected[5] - nstate[5])
+        elif (h_fb == H_UP or h_fb == H_DOWN):
+          cost = abs(state_corrected[3] - nstate[3])
+        else:
+          cost = abs(state_corrected[3] - nstate[3]) + abs(state_corrected[5] - nstate[5])
 
-        # Check cost
-        # if (h_fb == H_HOLD):
-        #   cost = abs(state_corrected[3] - nstate[3]) # Corrected Pole Angular Velocity (Zero)
-        # else: # LEFT or RIGHT
-        # cost = abs(state_corrected[2] - nstate[2]) # Corrected Velocity
-        cost = abs(state_corrected[0] - nstate_xpos) + abs(state_corrected[1] - nstate_ypos) # Corrected pole tip position
-        
         # Check for min_cost
         if(cost < min_cost):
           min_cost = cost
           min_action = curr_action
 
-      # Discrete actions: return a = A in one hot
-      min_a = np.zeros(self.action_dim)
-      min_a[min_action] = 1
-
-    return min_a
+    return min_action
 
   def get_action(self, state, nstate_required):
     """get action to achieve next state close to nstate_required"""
@@ -248,40 +272,45 @@ class TIPS_cartpole(TIPS):
     if (args.learnFDM):
       # Learnt FDM:
 
-      # x position frame correction!
+      # x-y position frame correction!
       nstate_required[0] -= state[0]
+      nstate_required[1] -= state[1]
       state[0] = 0
-      
+      state[1] = 0
+
       # Make a vector of same states
       States = np.tile(state, (self.ifdm_queries,1))
       # Choose random actions
-      # Discrete Actions
-      a = np.eye(self.action_dim)
-      Actions = a[np.random.choice(a.shape[0], size=self.ifdm_queries)]
+      # Continuous Actions
+      Actions = np.random.uniform(-1, 1, (self.ifdm_queries,self.action_dim) )
       # Query ifdm to get next state
       Nstates = self.eval_fdm(States, Actions)
 
       # Calculate cost
-      cost = np.sum(abs(nstate_required - Nstates[:]), axis=1) # Automatic broadcasting
+      # Automatic broadcasting
+      cost = np.sum(abs(nstate_required - Nstates[:]), axis=1)
 
       # Check for min_cost
       min_cost_index = cost.argmin(axis=0)
-      min_a = Actions[min_cost_index]
+      min_action = Actions[min_cost_index]
 
     else:
       # True FDM:
 
-      # Discrete Actions
-      min_action = np.random.randint(self.action_dim)
+      # Continous Actions
+      min_action = np.random.uniform(-1, 1, self.action_dim)
       min_cost = np.Inf
 
       for _ in range(1, self.ifdm_queries+1):
         # Choose random action
-        # Discrete Actions
-        curr_action = np.random.randint(self.action_dim)
+        # Continous Actions
+        curr_action = np.random.uniform(-1, 1, self.action_dim)
+        # Discretization
+        # val_set = [0.2*x for x in range(-5,6)]
+        # curr_action = np.random.choice(val_set, self.action_dim)
 
         # Query ifdm to get next state
-        nstate = fdm(state, curr_action)
+        nstate = fdm_cont(state, curr_action)
 
         # Check cost
         cost = sum(abs(nstate_required - nstate))
@@ -291,11 +320,7 @@ class TIPS_cartpole(TIPS):
           min_cost = cost
           min_action = curr_action
 
-      # Discrete actions: return a = A in one hot
-      min_a = np.zeros(self.action_dim)
-      min_a[min_action] = 1
-
-    return min_a
+    return min_action    
 
   def feedback_run(self):
     """run and train agent using D-COACH framework incorporating human feedback"""
@@ -327,7 +352,7 @@ class TIPS_cartpole(TIPS):
 
         # Get action from ifdm
         a = self.get_corrected_action(h_fb, state[0], state_corrected)
-        print("Computed Action: ", a)
+        # print("Computed Action: ", a)
 
         # Update policy (immediate)
         a = np.reshape(a, [-1, self.action_dim])
@@ -344,15 +369,22 @@ class TIPS_cartpole(TIPS):
 
         # Act using action based on h_feedback
         a = np.reshape(a, [-1])
-        # Discrete actions
-        A = np.argmax(a)
+        # Continuous actions
+        A = np.copy(a)
       else:
-        # Use current policy
+        if (args.learnFDM):
+          # Debug: equal timing
+          # time.sleep(0.02)
+          pass
+        else:
+          # Debug: equal timing
+          time.sleep(0.04)
 
+        # Use current policy
         # Map action from state
         a = np.reshape(self.eval_policy(state), [-1])
-        # Discrete actions
-        A = np.argmax(a)
+        # Continuous actions
+        A = np.copy(a)
 
       # Store previous state
       prev_s = np.copy(state)
@@ -363,11 +395,15 @@ class TIPS_cartpole(TIPS):
       state = np.reshape(state, [-1, self.state_dim])
 
       # Add to ExpBuff
-      # x position frame correction!
-      state[0,0] -= prev_s[0,0]
-      prev_s[0,0] = 0
+      # x-y position frame correction!
+      state_mod = np.copy(state)
+      prev_s_mod = np.copy(prev_s)
+      state_mod[0,0] -= prev_s_mod[0,0]
+      state_mod[0,1] -= prev_s_mod[0,1]
+      prev_s_mod[0,0] = 0
+      prev_s_mod[0,1] = 0
 
-      self.ExpBuff.append((prev_s[0], state[0], a))
+      self.ExpBuff.append((prev_s_mod[0], state_mod[0], a))
       if (len(self.ExpBuff) > self.maxExpBuffSize):
         self.ExpBuff.pop(0)
 
@@ -388,8 +424,8 @@ class TIPS_cartpole(TIPS):
 
     while not terminal:
       state = np.reshape(state, [-1,self.state_dim])
-      a = np.reshape(self.eval_policy(state), [-1])
-      A = np.argmax(a)
+      # Continuos action space
+      A = np.reshape(self.eval_policy(state), [-1])
       state, reward, terminal, _ = self.env.step(A)
       total_reward += reward
       if args.render:
@@ -399,5 +435,5 @@ class TIPS_cartpole(TIPS):
     return total_reward
     
 if __name__ == "__main__":
-  tips = TIPS_cartpole(4, 2, lr=args.lr, maxEpisodes=args.maxEpisodes)
+  tips = TIPS_lunarlandercont(8, 2, lr=args.lr, maxEpisodes=args.maxEpisodes)
   tips.run()
