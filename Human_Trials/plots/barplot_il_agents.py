52da20d8025dfba1f17d2b238113510a9c65dfbf
sjauhri <s.jauhri@student.tudelft.nl>
2020-04-01 09:57:29 +0200

    modified plot

--- Human_Trials/plots/barplot_il_agents.py
+++ Human_Trials/plots/barplot_il_agents.py
@@ -10,7 +10,7 @@ sns.set() # default seaborn theme, scaling, and color palette
 # CartPole
 cart_dict = [{'Method': 'Tele-Op (A)', 'Return': 55.4}, 
               {'Method': 'Tele-Op (S)', 'Return': 70.4},
-              {'Method': 'Behavior Cloning', 'Return': 138.2},
+              {'Method': 'Behavior Cloning*', 'Return': 108.2},
               {'Method': 'TIPS', 'Return': 200.0},
             #   {'Method': 'D-COACH', 'Return': 200.0},
               ]
@@ -19,7 +19,7 @@ cart = pd.DataFrame(cart_dict)
 # Reacher
 reach_dict = [{'Method': 'Tele-Op (A)', 'Return': -56.3}, 
               {'Method': 'Tele-Op (S)', 'Return': -36.1},
-              {'Method': 'Behavior Cloning', 'Return': -30.95},
+              {'Method': 'Behavior Cloning*', 'Return': -30.95},
               {'Method': 'TIPS', 'Return': -9.98},
             #   {'Method': 'D-COACH', 'Return': -19.2},
               ]
@@ -28,7 +28,7 @@ reach = pd.DataFrame(reach_dict)
 # LunarLander
 lunar_dict = [{'Method': 'Tele-Op (A)', 'Return': 5.4},
               {'Method': 'Tele-Op (S)', 'Return': 20.4},#90.4},
-              {'Method': 'Behavior Cloning', 'Return': -131.28},
+              {'Method': 'Behavior Cloning*', 'Return': -131.28},
               {'Method': 'TIPS', 'Return': 182.95},
             #   {'Method': 'D-COACH', 'Return': 205.58},
               ]
@@ -44,7 +44,7 @@ lunar['Return'] = (lunar['Return']+250).div(500) # normalize return
 # Plot using subplots
 figure, axes = plt.subplots(3, 1, sharey=True, figsize=(6.5, 9))
 plt.subplots_adjust(
-    left    =  0.19,
+    # left    =  0.19,
     # bottom  =  bottom, 
     # right   =  right, 
     # top     =  top, 
